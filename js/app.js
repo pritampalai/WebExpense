4c2b27361956846c4f6771bb755f5076d838323f
pritampalai <pritampalai@users.noreply.github.com>
2014-07-02 13:27:00 +0530

    commit

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ App.Router.map(function() {
 	  this.resource('eachexpense', {path: ':eachexpense_id' });
 	});
 	this.resource('addexpense', function() {
-	  this.resource('expense', {path: ':expense_id'}, function() {
+	  this.resource('expense', {path: ':exp_id'}, function() {
 	    this.route('editexpense');
 	  });
 	  this.route('new');
@@ -40,102 +40,44 @@ App.Person = DS.Model.extend( {
 	creationDate : DS.attr('date')           
 });
 
-App.Expenditure = DS.Model.extend( {
-    desc: DS.attr('string'),
-	date: DS.attr('date'),
-	whopaid: DS.attr('string'),
-	amount: DS.attr('integer'),
-	forwhom: DS.attr('string')
+App.Expenditure = DS.Model.extend( {   
+    desc      : DS.attr('string'),
+	date      : DS.attr('date'),
+	whopaid   : DS.attr('string'),
+	amount    : DS.attr('string'),
+	forwhom   : DS.attr('string')
 });
 
-App.AddexpenseRoute = Ember.Route.extend({
-   model: function(){  
-      var expenses = this.get('store').findAll('expenditure');	  
-	  return expenses;      
-   }   
-}); 
+// viewperson 
 
-App.AddexpenseController = Ember.ArrayController.extend({
-   sortProperties: ['amount'],
-   sortAscending: true,
-   
-   expensesCount: function(){
-      return this.get('model.length');
-   }.property('@each') 
+App.ViewpersonRoute = Ember.Route.extend({
+  model: function() {
+    var users = this.get('store').findAll('person');	
+	return users;
+  }
 });
 
-App.ExpenseRoute = Ember.Route.extend({
-   model: function(params){      
-        var store = this.get('store');
-		return store.find('expenditure',params.expenditure_id);		     
-   }
+App.EachuserRoute = Ember.Route.extend({
+  model: function(params) {
+    var store = this.get('store');
+		return store.find('person',params.person_id);
+  }
 });
 
-App.ExpenseController = Ember.ObjectController.extend({
-   deleteMode: false,
-           
-   actions: {
-      delete: function(){
-         this.toggleProperty('deleteMode');
-      },
-      cancelDelete: function(){         
-         this.set('deleteMode', false);
-      },
-      confirmDelete: function(){         
-         this.get('model').deleteRecord();
-         this.get('model').save();        
-         this.transitionToRoute('addexpense');        
-         this.set('deleteMode', false);
-      },
-      edit: function(){
-         this.transitionToRoute('expense.editexpense');
-      }
-   }
+App.EachuserController = Ember.ObjectController.extend({
+  isEditing: false,
+
+  edit: function() {
+    this.set('isEditing', true);
+  },
+
+  doneEditing: function() {
+    this.set('isEditing', false);
+    this.get('store').commit();
+  }
 });
 
-App.ExpenseEditexpenseRoute = Ember.Route.extend({
-   model: function(){ 
-      return this.modelFor('expense');
-   }
-});    
-
-App.ExpenseEditexpenseController = Ember.ObjectController.extend({
-   actions: {
-       save: function(){
-            var expense = this.get('model');
-            expense.save();
-            this.transitionToRoute('expense', expense);
-       }
-   }
-});
-
-App.AddexpenseNewRoute = Ember.Route.extend({
-   model: function(){  
-      var store = this.get('store');
-		return store.createRecord('expenditure',this.get('model'));      
-   },
-
-   renderTemplate: function(){
-      this.render('expense.editexpense', {
-          controller: 'addexpenseNew'
-      });
-   }
-});
-        
-App.AddexpenseNewController = Ember.ObjectController.extend({
-    actions: {
-        save: function () {    
-            this.get('model').set('date', new Date());
-			this.get('model').save();			            
-            this.transitionToRoute('addexpense');
-        }
-    }
-});
-
-
-
-
-
+// users
 
 App.UsersRoute = Ember.Route.extend({
    model: function(){  
@@ -227,13 +169,7 @@ App.UsersCreateuserController = Ember.ObjectController.extend({
 });
 
 
-
-
-
-
-
-
-
+// viewexpense
 
 App.ViewexpenseRoute = Ember.Route.extend({
    model: function() {
@@ -245,7 +181,7 @@ App.ViewexpenseRoute = Ember.Route.extend({
 App.EachexpenseRoute = Ember.Route.extend({
   model: function(params) {
     var store = this.get('store');
-		return store.find('expenditure',params.expenditure_id);
+		return store.find('expenditure',params.exp_id);
   }
 });
 
@@ -262,36 +198,96 @@ App.EachexpenseController = Ember.ObjectController.extend({
   }
 });
 
+// addexpense
 
+App.AddexpenseRoute = Ember.Route.extend({
+   model: function(){  
+      var expenses = this.get('store').findAll('expenditure');	  
+	  return expenses;      
+   }   
+}); 
 
-
-
-App.ViewpersonRoute = Ember.Route.extend({
-  model: function() {
-    var users = this.get('store').findAll('person');	
-	return users;
-  }
+App.AddexpenseController = Ember.ArrayController.extend({
+   sortProperties: ['amount'],
+   sortAscending: true,
+   
+   expensesCount: function(){
+      return this.get('model.length');
+   }.property('@each') 
 });
 
-App.EachuserRoute = Ember.Route.extend({
-  model: function(params) {
-    var store = this.get('store');
-		return store.find('person',params.person_id);
-  }
+App.ExpenseRoute = Ember.Route.extend({
+   model: function(params){      
+        var store = this.get('store');
+		return store.find('expenditure',params.exp_id);		     
+   }
 });
 
-App.EachuserController = Ember.ObjectController.extend({
-  isEditing: false,
-
-  edit: function() {
-    this.set('isEditing', true);
-  },
-
-  doneEditing: function() {
-    this.set('isEditing', false);
-    this.get('store').commit();
-  }
+App.ExpenseController = Ember.ObjectController.extend({
+   deleteMode: false,
+           
+   actions: {
+      delete: function(){
+         this.toggleProperty('deleteMode');
+      },
+      cancelDelete: function(){         
+         this.set('deleteMode', false);
+      },
+      confirmDelete: function(){         
+         this.get('model').deleteRecord();
+         this.get('model').save();        
+         this.transitionToRoute('addexpense');        
+         this.set('deleteMode', false);
+      },
+      edit: function(){
+         this.transitionToRoute('expense.editexpense');
+      }
+   }
 });
+
+App.ExpenseEditexpenseRoute = Ember.Route.extend({
+   model: function(){ 
+      return this.modelFor('expense');
+   }
+});    
+
+App.ExpenseEditexpenseController = Ember.ObjectController.extend({
+   actions: {
+       save: function(){
+            var expense = this.get('model');
+            expense.save();
+            this.transitionToRoute('expense', expense);
+       }
+   }
+});
+
+App.AddexpenseNewRoute = Ember.Route.extend({
+   model: function(){  
+      var store = this.get('store');
+		return store.createRecord('expenditure',this.get('model'));      
+   },
+
+   renderTemplate: function(){
+      this.render('expense.editexpense', {
+          controller: 'addexpenseNew'
+      });
+   }
+});
+        
+App.AddexpenseNewController = Ember.ObjectController.extend({
+    actions: {
+        save: function () {    
+            this.get('model').set('date', new Date());
+			this.get('model').save();			            
+            this.transitionToRoute('addexpense');
+        }
+    }
+});
+
+
+
+
+
 
 var showdown = new Showdown.converter();
 
